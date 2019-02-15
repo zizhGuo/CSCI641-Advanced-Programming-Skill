64ac4a794a99efd152e121541b029430e4cbd459
Zizhun Guo <zg2808@g.rit.edu>
2019-02-15 15:30:50 -0500

    Initialization with the first project: Hashtable with linkedlist entries

---
+++ project1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
